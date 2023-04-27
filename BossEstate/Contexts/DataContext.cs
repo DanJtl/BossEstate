abb518a35d6b88e54bdedfd14e2bf4a666ca8f57
Daniel Junttila <daniel_junttila@live.se>
2023-04-27 10:45:30 +0200

    Add DataContext.cs and constructors

---
+++ BossEstate/Contexts/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BossEstate.Contexts;
+
+internal class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    protected DataContext()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
