48bd2b47e8a1ac736905507f13277634377b3996
Daniel Junttila <daniel_junttila@live.se>
2023-05-02 14:16:53 +0200

    add StatusType.cs and add methods in CaseService.cs that inherit from GenericService.cs

--- BossEstate/Services/CaseService.cs
+++ BossEstate/Services/CaseService.cs
@@ -1,9 +1,35 @@
-﻿using BossEstate.Models.Entities;
+﻿using BossEstate.Contexts;
+using BossEstate.Models.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace BossEstate.Services;
 
 internal class CaseService : GenericService<CaseEntity>
 {
+    private readonly DataContext _context = new();
 
+    public override async Task<IEnumerable<CaseEntity>> GetAllAsync()
+    {
+        return await _context.Cases
+            .Include(x => x.User)
+            .Include(x => x.StatusType)
+            .ToListAsync();
+    }
+
+    public override async Task<CaseEntity> GetAsync(Expression<Func<CaseEntity, bool>> predicate)
+    {
+        var item = await _context.Cases
+            .Include(x => x.StatusType)
+            .Include(x => x.Comments)
+            .Include(x => x.User)
+            .Include(x => x.User).ThenInclude(x => x.Address)
+            .FirstOrDefaultAsync(predicate);
+        
+        if (item != null)
+            return item!;
+
+        return null!;
+
+    }
 }
---
+++ BossEstate/Services/StatusTypeService.cs
@@ -0,0 +1,7 @@
+﻿using BossEstate.Models.Entities;
+
+namespace BossEstate.Services;
+
+internal class StatusTypeService : GenericService<StatusTypeEntity>
+{
+}
