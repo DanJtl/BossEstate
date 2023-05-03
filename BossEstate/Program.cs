14680f926b273e3ae813283f662a83a1821cf0e0
Daniel Junttila <daniel_junttila@live.se>
2023-05-03 12:53:44 +0200

    Add method for displaying all data to console

--- BossEstate/Program.cs
+++ BossEstate/Program.cs
@@ -1,36 +1,9 @@
-﻿using BossEstate.Models.Entities;
-using BossEstate.Services;
+﻿using BossEstate.Services;
 
-var _caseService = new CaseService();
-var _datetime = DateTime.Now;
+var menu = new MenuService();
 
-var _case = new CaseEntity
+while (true)
 {
-    Id = Guid.NewGuid(),
-    Created = _datetime,
-    Modified = _datetime,
-    Title = "Trasig dörr",
-    Description = "Hantaget sitter löst",
-    StatusType = new StatusTypeEntity
-    {
-        StatusName = "Ej påbörjad"
-    },
-    User = new UserEntity
-    {
-        Id = Guid.NewGuid(),
-        FirstName = "Bat",
-        LastName = "Man",
-        Email = "test@gmail.com",
-        PhoneNumber = "1234567890",
-        Address = new AddressEntity
-        {
-            StreetName = "Batgatan 6",
-            PostalCode = "12355",
-            City = "BatCity"
-        }
-    }
-};
+    menu.MainMenu();
+}
 
-var result = await _caseService.SaveAsync(_case);
-
-Console.ReadKey();
--- BossEstate/Services/MenuService.cs
+++ BossEstate/Services/MenuService.cs
@@ -1,35 +1,30 @@
-﻿namespace BossEstate.Services;
+﻿using BossEstate.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BossEstate.Services;
 
 internal class MenuService
 {
+    private readonly UserService _userService = new();
+    private readonly CaseService _caseService = new();
+    private readonly AddressService _addressService = new();
+
+
     public void MainMenu()
     {
         Console.Clear();
         Console.WriteLine("******** Boss Estate ********");
         Console.WriteLine("*****************************");
-        Console.WriteLine(" [1] Create a New case.");
-        Console.WriteLine(" [2] View all Cases.");
-        Console.WriteLine(" [3] View a Specific case.");
-        Console.WriteLine(" [4] View all Users.");
-        Console.WriteLine(" [5] Update a specific case Status.");
-        Console.Write(" \nChoose an option: (1-5)");
+        Console.WriteLine(" [1] Show all data in local database");
+        Console.Write(" \nChoose: ");
         var option = Console.ReadLine();
 
         switch (option)
         {
             case "1":
-                break;
-
-            case "2":
-                break;
-
-            case "3":
-                break;
-
-            case "4":
-                break;
-
-            case "5":
+                
+                ShowAllData();
                 break;
 
             default:
@@ -37,6 +32,50 @@ internal class MenuService
                 Console.Write("No valid option! Please try again.");
                 break;
         }
+
         Console.ReadKey();
     }
+
+    public void ShowAllData()
+    {
+        var users = _userService.GetAllAsync().Result;
+        var addresses = _addressService.GetAllAsync().Result;
+        var cases = _caseService.GetAllAsync().Result;
+
+        Console.WriteLine("**** Users ****");
+        foreach (var user in users)
+        {
+            Console.WriteLine($"Id: {user.Id}");
+            Console.WriteLine($"First name: {user.FirstName}");
+            Console.WriteLine($"Last name: {user.LastName}");
+            Console.WriteLine($"Email: {user.Email}");
+            Console.WriteLine($"Phone number: {user.PhoneNumber}");
+            Console.WriteLine($"Address: {user.Address.StreetName}, {user.Address.PostalCode}, {user.Address.City}");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("**** Addresses ****");
+        foreach (var address in addresses)
+        {
+            Console.WriteLine($"Id: {address.Id}");
+            Console.WriteLine($"Street name: {address.StreetName}");
+            Console.WriteLine($"Postal code: {address.PostalCode}");
+            Console.WriteLine($"City: {address.City}");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("**** Cases ****");
+        foreach (var caseEntity in cases)
+        {
+            Console.WriteLine($"Id: {caseEntity.Id}");
+            Console.WriteLine($"Title: {caseEntity.Title}");
+            Console.WriteLine($"Description: {caseEntity.Description}");
+            Console.WriteLine($"Created: {caseEntity.Created}");
+            Console.WriteLine($"Modified: {caseEntity.Modified}");
+            Console.WriteLine($"User: {caseEntity.User.FirstName} {caseEntity.User.LastName}");
+            Console.WriteLine($"Status: {caseEntity.StatusType.StatusName}");
+            Console.WriteLine();
+        }
+    }
+   
 }
